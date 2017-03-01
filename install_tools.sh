bee7084dff718c520cb6e15c89385c43fe4b27a2
Jeremy Hale <jeremy.hale@gmail.com>
2017-03-02 06:27:25 +0900

    Install_tool.sh shows error if not MacOS

--- install_tools.sh
+++ install_tools.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-if [[ $(which brew) == "" ]]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+set -e
 
-bash tools/install/install_bazel.sh
-bash tools/install/install_buildifier.sh
+OSNAME=`uname -s`
+
+if [ "$OSNAME" == "Darwin" ]; then
+	if [[ $(which brew) == "" ]]; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+	bash tools/install/install_bazel.sh
+	bash tools/install/install_buildifier.sh
+elif [ "$OSNAME" == "Linux" ]; then
+	echo "Not implemented for Linux"
+	exit 1
+else
+	echo "Not implemented for $OSNAME"
+	exit 1
+fi
 
 echo "Finished."
